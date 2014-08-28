607049f5422596099bb2d1d0b56145fb3b136b53
Ben Everard <hello@beneverard.co.uk>
2014-08-28 21:18:14 +0100

    Added functions to remove YARPP styling + javascript

--- functions/plugins.php
+++ functions/plugins.php
@@ -4,3 +4,13 @@
 add_filter('wpseo_metabox_prio', function() {
 	return 'low';
 });
+
+// remove YARPP CSS files
+
+add_action('wp_head', function() {
+	wp_dequeue_style('yarppWidgetCss');
+}, 1);
+
+add_action('wp_footer', function() {
+	wp_dequeue_style('yarppRelatedCss');
+}, 1);
