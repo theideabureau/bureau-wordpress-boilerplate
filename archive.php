881d6bc627dbfa4d015989d2c8b3bd9e573ece59
Ben Everard <hello@beneverard.co.uk>
2014-08-28 18:20:21 +0100

    Output archive post within article element

--- archive.php
+++ archive.php
@@ -22,9 +22,13 @@
 
 	<?php if ( have_posts() ) : the_post(); ?>
 
-		<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+		<article>
 
-		<?php the_content(); ?>
+			<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+
+			<?php the_content(); ?>
+
+		</article>
 
 	<?php endif; ?>
 
