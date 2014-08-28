99a97d1a6937ccb0893714619de0f2ecdd7850e1
Ben Everard <hello@beneverard.co.uk>
2014-08-28 20:58:15 +0100

    Added ACF sub-options pages

--- functions/acf-fields.php
+++ functions/acf-fields.php
@@ -1,1 +1,13 @@
 <?php // functions/acf-fields.php
+
+/**
+ * Adds various sub-options pages
+ */
+//add_filter('acf/options_page/settings', function($settings) {
+//
+// 	$settings['title'] = 'Options';
+// 	$settings['pages'] = array('General', 'Social', 'Contact', 'Subscription', 'Menu', 'Adverts');
+//
+// 	return $settings;
+//
+// });
