963259cb865e0b62e7f5e8113ecf0a2e8006e8d6
Connor Holyday <connorholyday@gmail.com>
2015-09-17 16:53:50 +0100

    Disable emoji

--- functions/admin.php
+++ functions/admin.php
@@ -162,3 +162,22 @@ add_filter('admin_footer_text', 'remove_footer_admin');
 function remove_footer_admin () {
 	echo "Designed and Built by <a href='http://theideabureau.co' target='_blank'>The Idea Bureau</a>";
 }
+
+ //**************
+// DISABLE EMOJI
+
+function disable_wp_emojicons() {
+
+  // all actions related to emojis
+  remove_action( 'admin_print_styles', 'print_emoji_styles' );
+  remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+  remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+  remove_action( 'wp_print_styles', 'print_emoji_styles' );
+  remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+  remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+  remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
+
+  // filter to remove TinyMCE emojis
+  add_filter( 'tiny_mce_plugins', 'disable_emojicons_tinymce' );
+}
+add_action( 'init', 'disable_wp_emojicons' );
