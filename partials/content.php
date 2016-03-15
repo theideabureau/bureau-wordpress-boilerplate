cc51f3fcedecb879761cea29399f2af5977f0ea1
Ben Everard <ben@theideabureau.co>
2016-03-15 16:37:11 +0000

    Added correct comment template output

--- partials/content.php
+++ partials/content.php
@@ -4,5 +4,9 @@
 
 	<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
 	<?php the_content(); ?>
+	
+	<?php if ( comments_open() || '0' != get_comments_number() ) : ?>
+		<?php comments_template(); ?>
+	<?php endif; ?>
 
 <?php endwhile; ?>
